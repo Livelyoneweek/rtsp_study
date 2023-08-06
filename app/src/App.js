8f7e5ea477a63506080caf1ca661d3dc9a317606
Livelyoneweek <wlvvwhs@naver.com>
2023-08-06 19:41:52 +0900

    fix:fix

--- app/src/App.js
+++ app/src/App.js
@@ -1,18 +1,21 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import JSMpeg from "@cycjimmy/jsmpeg-player";
 
 const ffmpegIP = "localhost";
 
 const App = () => {
-  useEffect(() => {
+  const [player, setPlayer] = useState(null);
+
+  const startPlayer = () => {
     var videoUrl = `ws://${ffmpegIP}:6789/`;
     var player = new JSMpeg.VideoElement("#video-canvas", videoUrl, {
       autoplay: true,
       bufferSize: 2048 * 2048, // 원하는 버퍼 크기로 설정 (디폴트: 512kb)
     });
+    setPlayer(player);
     console.log(player);
-  });
+  };
 
   return (
     <div id="body">
@@ -31,9 +34,51 @@ const App = () => {
       >
         Player stream RTSP
       </div>
+      <button onClick={startPlayer}>Start Streaming</button>
       <div id="video-canvas" style={{ height: "480px", width: "640px" }}></div>
     </div>
   );
 };
 
 export default App;
+
+// import React, { useEffect, useRef } from "react";
+// import "./App.css";
+
+// const App = () => {
+//   const videoRef = useRef(null);
+
+//   const startPlayer = () => {
+//     const videoUrl = `ws://localhost:6789/`;
+//     videoRef.current.src = videoUrl;
+//     videoRef.current.play();
+//   };
+
+//   return (
+//     <div id="body">
+//       <div
+//         id="title"
+//         style={{
+//           display: "flex",
+//           justifyContent: "center",
+//           alignItems: "center",
+//           fontSize: "28px",
+//           fontWeight: "bold",
+//           marginTop: "10px",
+//           marginBottom: "10px",
+//           color: "blue",
+//         }}
+//       >
+//         Player stream RTSP
+//       </div>
+//       <button onClick={startPlayer}>Start Streaming1</button>
+//       <video
+//         ref={videoRef}
+//         style={{ height: "480px", width: "640px" }}
+//         controls
+//       ></video>
+//     </div>
+//   );
+// };
+
+// export default App;
--- node/app.js
+++ node/app.js
@@ -2,21 +2,12 @@ Stream = require("node-rtsp-stream");
 stream = new Stream({
   name: "Bunny",
   // streamUrl: "rtsp://YOUR_IP:PORT",
-  //streamUrl: "rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mp4",
-  streamUrl: "rtsp://210.99.70.120:1935/live/cctv001.stream",
+  // streamUrl: "rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mp4",
+  streamUrl: "rtsp://210.99.70.120:1935/live/cctv002.stream",
   wsPort: 6789,
   ffmpegOptions: { // options ffmpeg flags
-    "-f": "mpegts", // output file format.
-    "-vcodec": "mpeg1video", // video codec
-    "-s": "640x480", // video size
-    "-b:v": "1000k", // video bitrate
-    "-r": "30", // video framerate
-    "-bf": "0", // B-frames
-    "-codec:a": "mp2", // audio codec
-    "-ar": "44100", // audio sample rate
-    "-ac": "1", // audio channels
-    "-b:a": "128k", // audio bitrate
-    '-stats': '', // an option with no neccessary value uses a blank string
+    // "-f": "mpegts", // output file format.
+    // "-rtsp_transport": "tcp",
   },
 });
 
@@ -27,3 +18,84 @@ stream.on('ffmpegError', function(data) {
 stream.on('ffmpegOutput', function(data) {
   console.log('FFmpeg Output:', data);
 });
+
+
+// const { spawn } = require('child_process');
+
+// const ffmpegPath = '/opt/homebrew/bin/ffmpeg'; // FFmpeg 실행 파일 경로
+// const inputRTSPStream = 'rtsp://210.99.70.120:1935/live/cctv002.stream'; // 입력 RTSP 스트림 URL
+// const outputWebSocketURL = 'ws://localhost:6789'; // WebSocket 서버 URL
+// const options = [
+//   '-i', inputRTSPStream,
+//   '-vsync', 'vfr', // VFR 영상 처리를 위한 옵션
+//   '-f', 'mpegts',
+//   '-codec:v', 'mpeg1video',
+//   '-s', '960x540',
+//   '-b:v', '1000k',
+//   '-r', '50',
+//   '-bf', '0',
+//   '-codec:a', 'mp2',
+//   '-ar', '44100',
+//   '-ac', '1',
+//   '-b:a', '128k',
+//   '-'
+// ];
+
+// const express = require('express');
+// const http = require('http');
+// const WebSocket = require('ws');
+// const { spawn } = require('child_process');
+
+// const app = express();
+// const server = http.createServer(app);
+// const wss = new WebSocket.Server({ server });
+
+// const ffmpegIP = 'localhost';
+// const ffmpegPort = 6789;
+// const rtspUrl = 'rtsp://210.99.70.120:1935/live/cctv002.stream';
+// const ffmpegArgs = [
+//   '-rtsp_transport', 'tcp',
+//   '-i', rtspUrl,
+//   '-f', 'mpegts',
+//   '-codec:v', 'mpeg1video',
+//   '-s', '960x540',
+//   '-b:v', '1000k',
+//   '-r', '50',
+//   '-bf', '0',
+//   '-codec:a', 'mp2',
+//   '-ar', '44100',
+//   '-ac', '1',
+//   '-b:a', '128k',
+//   '-stats',
+//   '-vsync', 'vfr', // Use vfr vsync for smoother output
+//   'pipe:1', // Output to stdout
+// ];
+
+// // WebSocket Server
+// wss.on('connection', (ws) => {
+//   console.log('WebSocket Client Connected');
+
+//   // Spawn FFmpeg process
+//   const ffmpegProcess = spawn('ffmpeg', ffmpegArgs);
+
+//   // Pipe FFmpeg output to WebSocket client
+//   ffmpegProcess.stdout.pipe(ws);
+
+//   // Handle WebSocket close event
+//   ws.on('close', () => {
+//     console.log('WebSocket Client Disconnected');
+//     ffmpegProcess.kill(); // Kill FFmpeg process when WebSocket client disconnects
+//   });
+// });
+
+// const PORT = process.env.PORT || 3000;
+// server.listen(PORT, () => {
+//   console.log(`Server started on port ${PORT}`);
+// });
+
+
+
+
+
+
+
